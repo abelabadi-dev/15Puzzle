badb57e453b6a5e7d7df308304e98b776a5aa9ea
abel abadi <abelabadi2@gmail.com>
2016-05-13 14:41:25 -0500

    setup game area and game pieces.
    setup the background image and divided up the tiles from jquery!

---
+++ js/fifteen.js
@@ -0,0 +1,30 @@
+/*
+	@abule
+*/
+
+//document.ready
+$(function () {
+	init();
+});
+
+init = function() {
+    var puzzleArea = $('#puzzlearea');
+    var divs = puzzleArea.children("div");
+      
+    // initialize each piece
+    divs.each(function (index) {
+    	var x = ((index%4)*100);
+    	var y = (Math.floor(index / 4) * 100);
+    	var $this = $(this);
+    	$this.addClass("puzzlepiece");
+    	$this.css({"left" : x + 'px',
+		    		"top" :y + 'px',
+		    		"backgroundImage" : 'url("img/bg.jpg")',
+		    		"backgroundPosition" : -x + 'px ' + (-y) + 'px'
+    	});
+
+    	$this.x = x;
+    	$this.y = y;
+    });
+           
+};
