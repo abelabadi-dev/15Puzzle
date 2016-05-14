c57702bc5a34c47f127a5ec548322b3709409a0b
abel abadi <abelabadi2@gmail.com>
2016-05-14 10:24:06 -0500

    add ids to the pieces
    pieces row_column id to identify

--- js/fifteen.js
+++ js/fifteen.js
@@ -4,27 +4,31 @@
 
 //document.ready
 $(function () {
-	init();
+	Puzzle.init();
 });
+var Puzzle = {
+    init: function() {
+        var puzzleArea = $('#puzzlearea');
+        var divs = puzzleArea.children("div");
+          
+        // initialize each piece
+        divs.each(function (index) {
+            var x = ((index%4)*100);
+            var y = (Math.floor(index / 4) * 100);
+            var $this = $(this);
+            $this.addClass("puzzlepiece");
+            $this.css({"left" : x + 'px',
+                        "top" :y + 'px',
+                        "backgroundImage" : 'url("img/bg.jpg")',
+                        "backgroundPosition" : -x + 'px ' + (-y) + 'px'
+            });
 
-init = function() {
-    var puzzleArea = $('#puzzlearea');
-    var divs = puzzleArea.children("div");
-      
-    // initialize each piece
-    divs.each(function (index) {
-    	var x = ((index%4)*100);
-    	var y = (Math.floor(index / 4) * 100);
-    	var $this = $(this);
-    	$this.addClass("puzzlepiece");
-    	$this.css({"left" : x + 'px',
-		    		"top" :y + 'px',
-		    		"backgroundImage" : 'url("img/bg.jpg")',
-		    		"backgroundPosition" : -x + 'px ' + (-y) + 'px'
-    	});
+            $this.x = x;
+            $this.y = y;
+            console.log($this.attr('id',x +"_"+y));
+        });       
+    },
+    moveIt(){
 
-    	$this.x = x;
-    	$this.y = y;
-    });
-           
+    }
 };
